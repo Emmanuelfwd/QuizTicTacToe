c84ba8b16f6c3b6dfa350d838a1d096a677bf958
Emmanuelfwd <eruizhurtado25fwd@gmail.com>
2025-08-18 15:27:20 -0600

    faltando media hora para salir

---
+++ src/script/script.js
@@ -0,0 +1,18 @@
+let text = document.getElementById("enunciado");
+let btnReset = document.getElementById("btnRestart");
+let boxes = Array.from( document.getElementsByClassName("box"));
+
+let jugador1 = "X";
+let jugador2 = "O";
+let currentPlayer = jugador1;
+let posicion = Array(9).fill(null);
+
+const comenzar = () => {
+    boxes.forEach(box => box.addEventListener('click',rellenada))
+}
+
+function rellenada(e) {
+    console.log(e.target);
+}
+
+comenzar();
